4a42c299d39c02a5903325bd6d0636b0fb99c2a7
MikeJMS8910 <61876275+MikeJMS8910@users.noreply.github.com>
2023-08-18 20:30:38 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -46,6 +46,16 @@ const validateEmail = (email) => {
     }
 }
 
+/* Function to valide phone numbers. Takes in a string and returns whether it is a valid phone number. */
+const validatePhone = (phone) => {
+    try {
+        const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+        return {code: 200, result: phone.match(phoneRegex) }
+    } catch (err) {
+        return {code: 500, err: err}
+    }
+}
+
 /* Function to create a new value in MongoDB. Takes in collection name and data to add. */
 const dbCreate = async (collection, data) => {
     try {
@@ -117,11 +127,11 @@ const dbUpdatePush = async (collection, query, data) => {
 }
 
 /* Function that returns the new userid based on previous ones in the MongoDB database. */
-const getNewUserId = async () => {
+const getNewUserId = async (email) => {
     try {
         await client.connect()
 	    const db = client.db("main")
-        const resultCursor = await db.collection("waitlist").find().sort({userId: -1}).limit(1)
+        const resultCursor = await db.collection("waitlist").find().sort({userId: -1})
         const result = await resultCursor.toArray()
 
         let highestUserId = 0
@@ -129,7 +139,34 @@ const getNewUserId = async () => {
             highestUserId = result[0].userId
         }
         
-        return {code: 200, result: highestUserId + 1}
+        if(email) {
+            let possibleNewId = -1
+            for(let i = 0; i < result.length; i++) {
+                if(result[i].email == email) {
+                    possibleNewId = result[i].userId
+                }
+            }
+
+            if(possibleNewId == -1) {
+                const usersResultCursor = await db.collection("users").find().sort({userId: -1}).limit(1)
+                const userResult = await usersResultCursor.toArray()
+
+                let usersHighestUserId = 0
+                if(userResult.length > 0) {
+                    usersHighestUserId = userResult[0].userId
+                }
+
+                if(usersHighestUserId > highestUserId) {
+                    return {code: 200, result: usersHighestUserId + 1}
+                } else {
+                    return {code: 200, result: highestUserId + 1}
+                }
+            } else {
+                return {code: 200, result: possibleNewId}
+            }
+        } else {
+            return {code: 200, result: highestUserId + 1}
+        }
     } catch(err) {
         return {code: 500, err: err}
     } finally {
@@ -256,6 +293,118 @@ app.post("/api/getjoined", async (req, res) => {
         res.json({code: 500, err: err})
     }
 })
+
+/* Post route sign up a new user account. */
+app.post("/api/signup", async (req, res) => {
+    try {
+        const firstName = req.body.firstName
+        const lastName = req.body.lastName
+        const email = req.body.email.toLowerCase()
+        const role = req.body.role
+        const password = req.body.password
+        const userId = await getNewUserId(email)
+        const private = randomId(15)
+        let phone = ""
+        if(req.body.phone) {
+            const testPhone = validatePhone(req.body.phone)
+            if(testPhone.result != null) {
+                phone = req.body.phone
+            }
+        }
+        let errors = []
+
+        if(firstName == "" || firstName == null) {
+            errors.push([1, "First name required."])
+        }
+
+        if(lastName == "" || lastName == null) {
+            errors.push([2, "Last name required."])
+        }
+
+        if(email == "" || email == null) {
+            errors.push([3, "Email required."])
+        } else {
+            const testEmail = validateEmail(email)
+            if(testEmail.result == null) {
+                errors.push([4, "Invalid email."])
+            }
+
+            const getEmail = await dbGet("users", {"email": email})
+            if(getEmail.result != null) {
+                errors.push([5, "Email taken."])
+            }
+        }
+
+        if(role != "developer" && role != "buyer" && role != "both") {
+            errors.push([6, "Invalid role."])
+        }
+
+        if(password == "" || password == null) {
+            errors.push([7, "Password required."])
+        }
+
+        if(errors.length > 0) {
+            res.json({code: 401, errors: errors})
+        } else {
+            bcrypt.genSalt(10, async (err, salt) => {
+                bcrypt.hash(password, salt, async function(err, hash) {
+                    const result = await dbCreate("users", {
+                        firstName: firstName,
+                        lastName: lastName,
+                        email: email,
+                        role: role,
+                        userId: userId.result,
+                        private: private.result,
+                        password: hash,
+                        phone: phone
+                    })
+        
+                    if(result.code == 200) {
+                        result.private = private.result
+                        res.json(result)
+                    } else {
+                        res.json(result)
+                    }
+                })
+            })
+        }
+    } catch(err) {
+        res.json({code: 500, err: err})
+    }
+})
+
+/* Post route to login a user. */
+app.post("/api/login", async (req, res) => {
+    try {
+        let result = await dbGet("users", {email: req.body.email})
+        result = result.result
+        if(result) {
+            bcrypt.compare(req.body.password, result.password, (err, pass) => {
+                if (err) {
+                    res.json({code: 500, err: err})
+                }
+    
+                if (pass) {
+                    res.json({code: 200, result: result.private})
+                } else {
+                    res.json({code: 401, errors: [2, "Invalid password."]})
+                }
+            })
+        } else {
+            res.json({code: 401, errors: [1, "Unknown email."]})
+        }
+    } catch(err) {
+        res.json({code: 500, err: err})
+    }
+})
+
+/*
+Upload to github
+Restart server
+Test
+Share docs with designer
+Set up new mongo stats for # in users and types in users
+*/
 
 /* Get route that returns a basic html page. */
 app.get("/", (req, res) => {
